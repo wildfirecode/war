eeadca0f669af3d9a6d7faf1295329e865b37b04
wildfirecode <wildfirecode13@gmail.com>
2022-06-20 18:03:06 +0800

    使用createWithSpriteFrames创建动画

--- assets/script/loading.ts
+++ assets/script/loading.ts
@@ -1,53 +1,27 @@
-import { Component, resources, Sprite, SpriteFrame, _decorator, Node, Layers, ImageAsset, assetManager, SpriteAtlas, Asset } from 'cc';
-import FrameAnimation from '../lib/frameAnimation';
+import { Animation, AnimationClip, Component, Layers, Node, Sprite, _decorator } from 'cc';
 import { loadAtlas } from '../utils/loadAtlas';
 const { ccclass, property } = _decorator;
 
 @ccclass('loading')
 export class loading extends Component {
     start() {
+        const animationNode = new Node();
+        animationNode.addComponent(Sprite);
+        animationNode.layer = Layers.Enum.UI_2D;
+        this.node.addChild(animationNode);
+        animationNode.addComponent(Animation);
 
-        const animNode = new Node();
-        animNode.addComponent(Sprite);
-        animNode.layer = Layers.Enum.UI_2D;
-        this.node.addChild(animNode);
-        animNode.addComponent(FrameAnimation);
-
-        // 加载 SpriteAtlas（图集），并且获取其中的一个 SpriteFrame
-        // 注意 atlas 资源文件（plist）通常会和一个同名的图片文件（png）放在一个目录下, 所以需要在第二个参数指定资源类型
-        // resources.load("hero", SpriteAtlas, (err, atlas) => {
-        //     const frameAnimation = animNode.getComponent(FrameAnimation);
-        //     frameAnimation.setFrameArray(atlas.getSpriteFrames());
-        //     frameAnimation.play(!!1);
-        // });
-
-        const remoteUrl = 'http://127.0.0.1:8080/hero';
+        const remoteUrl = 'http://10.42.0.244:8080/hero';
         loadAtlas(remoteUrl).then((atlas) => {
-            const frameAnimation = animNode.getComponent(FrameAnimation);
-            frameAnimation.setFrameArray(atlas.getSpriteFrames());
-            frameAnimation.play(!!1);
+            const frames = atlas.getSpriteFrames();
+            const fps = 10;
+            const clip = AnimationClip.createWithSpriteFrames(frames, fps);
+            clip.name = 'standby';
+            clip.wrapMode = 2;//循环
+            const animation = animationNode.getComponent(Animation);
+            animation.defaultClip = clip;
+            animation.play('standby');
         });
-
-
-        // const pic = new Node();
-        // pic.addComponent(Sprite);
-        // pic.layer = Layers.Enum.UI_2D;
-        // this.node.addChild(pic);
-        // const sprite = pic.getComponent(Sprite);
-
-        // // const url = 'background/spriteFrame';
-        // // resources.load(url, SpriteFrame, (err: any, spriteFrame) => {
-        // //     sprite.spriteFrame = spriteFrame;
-        // // });
-
-        // const remoteUrl = 'http://127.0.0.1:8080/hero.png';
-        // assetManager.loadRemote<ImageAsset>(remoteUrl, function (err, imageAsset) {
-        //     sprite.spriteFrame = SpriteFrame.createWithImage(imageAsset)
-        // });
-
-    }
-
-    update(deltaTime: number) {
 
     }
 }
