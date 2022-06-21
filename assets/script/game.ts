f5216044c277a189e3a78e649b82ef8660766d84
wildfirecode <wildfirecode13@gmail.com>
2022-06-21 11:01:38 +0800

    设置设计分辨率

--- assets/script/game.ts
+++ assets/script/game.ts
@@ -9,37 +9,46 @@ const { ccclass, property } = _decorator;
 export class game extends Component {
     start() {
 
+        this.createBackground();
+
+        const hero = loadRemoteModel('http://10.42.0.244:8080/model/hero');
+        this.node.addChild(hero);
+
+
+
+
+        // const getpos = () => {
+        //     const pos = 300;
+        //     const x = (Math.random() * pos * 2 - pos);
+        //     const y = (Math.random() * pos * 2 - pos);
+        //     return { x, y }
+        // }
+        // const loadremote = location.href.indexOf('loadremote') != -1;
+        // //load远程资源
+        // if (loadremote) {
+        //     const resourcesUrl = 'http://10.42.0.244:8080/model/enemy3_down';
+        //     for (let index = 0; index < 100; index++) {
+        //         const model = loadRemoteModel(resourcesUrl);
+        //         this.node.addChild(model);
+        //         const pos = getpos();
+        //         model.position.set(pos.x, pos.y, 0);
+        //     }
+        // } else {
+        //     const resourcesUrl = 'model/enemy3_down';
+        //     //load本地资源
+        //     for (let index = 0; index < 100; index++) {
+        //         const model = loadModel(resourcesUrl);
+        //         this.node.addChild(model);
+        //         const pos = getpos();
+        //         model.position.set(pos.x, pos.y, 0);
+        //     }
+        // }
+    }
+
+    private createBackground() {
         const remoteUrl = 'http://10.42.0.244:8080/ui/background.png';
-        const image = loadRemoteImage(remoteUrl)
+        const image = loadRemoteImage(remoteUrl);
         this.node.addChild(image);
-
-
-        const getpos = () => {
-            const pos = 300;
-            const x = (Math.random() * pos * 2 - pos);
-            const y = (Math.random() * pos * 2 - pos);
-            return { x, y }
-        }
-        const loadremote = location.href.indexOf('loadremote') != -1;
-        //load远程资源
-        if (loadremote) {
-            const resourcesUrl = 'http://10.42.0.244:8080/model/enemy3_down';
-            for (let index = 0; index < 100; index++) {
-                const model = loadRemoteModel(resourcesUrl);
-                this.node.addChild(model);
-                const pos = getpos();
-                model.position.set(pos.x, pos.y, 0);
-            }
-        } else {
-            const resourcesUrl = 'model/enemy3_down';
-            //load本地资源
-            for (let index = 0; index < 100; index++) {
-                const model = loadModel(resourcesUrl);
-                this.node.addChild(model);
-                const pos = getpos();
-                model.position.set(pos.x, pos.y, 0);
-            }
-        }
     }
 }
 
